b3c58e8a4bbddec60fe942253120091721e19f13
reco_luan <recoluan@outlook.com>
2019-10-21 19:55:40 +0800

    feat: add pwa
    Edit develop doc.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,12 +7,31 @@ module.exports = {
   // base: '/vuepress-theme-reco-doc/',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#42b983' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icon_vuepress_reco.png' }],
+    ['link', { rel: 'mask-icon', href: '/icon_vuepress_reco.svg', color: '#42b983' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icon_vuepress_reco.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   theme: 'reco',
   themeConfig,
   markdown: {
     lineNumbers: true
   },
-  plugins: ['flowchart'] 
+  plugins: [
+    [
+      '@vuepress/pwa', 
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "发现新内容可用",
+          buttonText: "刷新"
+        }
+      }
+    ]
+  ]
 }  
