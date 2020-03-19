d87981d78f504ccd86c9979639da923e018cc576
reco_luan <recoluan@outlook.com>
2020-03-19 12:50:26 +0800

    push

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -20,7 +20,8 @@ module.exports = {
     { text: '常见问题', link: '/views/other/question', icon: 'reco-faq' },
     { text: '博客', link: '/categories/blog/', icon: 'reco-blog'},
     { text: '案例', link: '/views/other/theme-example.html', icon: 'reco-category'},
-    { text: '订阅', link: 'https://vuepress-theme-reco.recoluan.com/rss.xml', icon: 'reco-rss'}
+    { text: '订阅', link: 'https://vuepress-theme-reco.recoluan.com/rss.xml', icon: 'reco-rss'},
+    { text: 'GitHub', link: 'https://github.com/vuepress-reco/vuepress-theme-reco', icon: 'reco-github'}
   ],
   'en': [
     { text: 'Documents', 
@@ -40,6 +41,7 @@ module.exports = {
     },
     { text: 'FAQ', link: '/en/views/other/question', icon: 'reco-faq' },
     { text: 'Examples', link: '/en/views/other/theme-example.html', icon: 'reco-category'},
-    { text: 'RSS', link: 'https://vuepress-theme-reco.recoluan.com/rss.xml', icon: 'reco-rss'}
+    { text: 'RSS', link: 'https://vuepress-theme-reco.recoluan.com/rss.xml', icon: 'reco-rss'},
+    { text: 'GitHub', link: 'https://github.com/vuepress-reco/vuepress-theme-reco', icon: 'reco-github'}
   ]
 }
