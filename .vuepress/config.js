e7d43c163e3aa38b6faed09ab963a891fa8836ba
reco_luan <recoluan@outlook.com>
2021-09-20 14:23:11 +0800

    push

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -91,7 +91,12 @@ module.exports = {
         },
         {
           type: 'text',
-          content: '🎉🎉🎉 reco 主题 2.x 已经进入准备工作，留下你的想法 <a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/>。',
+          content: `🎉🎉🎉 reco 主题 2.x 已经发布 alpha 版本，此版本仅为尝鲜版本，功能不完整，且 UI 及功能在 latest 版本之前均为做出较大，谨慎用于生产环境。
+          <ul>
+            <li><a href="/views/2.x/">Usage<a/></li>
+            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+          </ul>`,
           style: 'font-size: 12px;'
         },
         // {
---
+++ views/2.x/README.md
@@ -0,0 +1,33 @@
+---
+title: 介绍
+date: 2021-09-20
+---
+
+::: warning
+此版本仅为尝鲜版本，功能不完整，且 UI 及功能在 latest 版本之前均为做出较大，谨慎用于生产环境。
+:::
+
+## 快速开始
+
+**npx**
+
+```
+# 初始化，并选择 2.x
+npx @vuepress-reco/theme-cli init
+```
+
+**npm**
+
+```bash
+# 初始化，并选择 2.x
+npm install @vuepress-reco/theme-cli@1.0.7 -g
+theme-cli init
+```
+
+**yarn**
+
+```bash
+# 初始化，并选择 2.x
+yarn global add @vuepress-reco/theme-cli@1.0.7
+theme-cli init
+```
