198dd56c8de915ef9f25f9ad43a23d314f14d211
reco_luan <recoluan@outlook.com>
2021-09-20 19:57:04 +0800

    push

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,11 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:recoluan/vuepress-reco-doc.git master
+# git push -f git@github.com:recoluan/vuepress-reco-doc.git master
 
-git push -f git@git.coding.net:recoluan/vuepress-theme-reco-doc.git master
+# git push -f git@git.coding.net:recoluan/vuepress-theme-reco-doc.git master
+
+git push -f git@github.com:vuepress-reco/vuepress-reco.github.io.git master
 
 cd ../
 rm -rf public
